68e969b9467f644bf4bd22f8c1141e5363a3373c
Maël Le Garrec <mlegarrec@online.net>
2018-05-03 13:35:07 +0000

    convert image to new build system

---
+++ latest/env.mk
@@ -0,0 +1,6 @@
+IMAGE_NAME =			gitlab
+IMAGE_VERSION =		latest
+IMAGE_VERSION_ALIASES =	
+IMAGE_TITLE =			GitLab
+IMAGE_DESCRIPTION =		GitLab host your private and public software projects 
+IMAGE_SOURCE_URL =		https://github.com/scaleway-community/scaleway-gitlab
